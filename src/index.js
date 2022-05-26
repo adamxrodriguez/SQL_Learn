7b80f66511e041517068cdb971dddfeb94a071a8
Adam Rodriguez <adamxrodriguez@gmail.com>
2022-05-26 00:49:44 -0400

    late night

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 'use strict';
 
-
-
 // import MainContainer from "../containers/MainContainer";
 
-// import React, { Component } from 'react';
+import React, { Component } from 'react';
 // import { connect } from 'react-redux';
 // import MainContainer from "../containers/MainContainer";
 
